75f0ab406e1c55f19d0629f16c62bbee46ceaafb
AnkitAnandMastery17 <7teenfootballer.05@gmail.com>
2024-11-29 17:50:48 +0530

    Initial commit

---
+++ dist/script.js
@@ -0,0 +1,35 @@
+gsap.to(".slide",{
+    transform:'translate(-300%)',
+    duration:2,
+    scrollTrigger:{
+        trigger:".slide",
+        markers:true,
+        start:"top 0",
+        end:"+=1900",
+        scrub:0.3
+    }
+})
+
+
+// var tl = gsap.timeline()
+
+// tl.to(".banner-img",{
+//     transform:'translateX(-100%)',
+//     duraton:1,
+//     ease:"ease"
+// },"+=3")
+// tl.to(".banner-img",{
+//     transform:'translateX(-200%)',
+//     duraton:1,
+//     ease:"power4"
+// },"+=3")
+// tl.to(".banner-img",{
+//     transform:'translateX(-300%)',
+//     duraton:1,
+//     ease:"power4"
+// },"+=3")
+// tl.to(".banner-img",{
+//     transform:'translateX(-400%)',
+//     duraton:1,
+//     ease:"power4"
+// },"+=3")
